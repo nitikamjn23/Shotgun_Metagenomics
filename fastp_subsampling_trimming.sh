ad4eab087a8112ae60af2efb7c4972a02ad16d78
nitikamjn23 <nitika.mjn23@gmail.com>
2026-07-29 15:30:17 +0200

    Add fastp subsampling trimming script
    This script activates a conda environment, creates a directory for results, and runs fastp for trimming subsampled reads.

---
+++ fastp_subsampling_trimming.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+source /lustrehome/nitika/miniconda3/bin/activate
+
+
+conda activate pipeline
+
+
+mkdir -p fastp_subsampling_trimmed_results
+
+
+fastp  -i /lustrehome/nitika/bash_script/fastq_file/subsampled_reads/SRR25132914_R1*.fastq -I /lustrehome/nitika/bash_script/fastq_file/subsampled_reads/SRR25132914_R2*.fastq -o SRR25132914_R1_trimmed*.fastq.gz -O SRR25132914_R2_trimmed*.fastq.gz --detect_adapter_for_pe --length_required 45 -q 20 -h SRR25132914_fastp_report.html -j SRR25132914_fastp_report.json
+
+conda deactivate pipeline
