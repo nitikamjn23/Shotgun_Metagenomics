c3e5f263d2590a474b2e69ea8497c152b3cb2673
nitikamjn23 <nitika.mjn23@gmail.com>
2026-08-06 11:04:38 +0200

    Update comments in kraken2.sh

--- kraken2.sh
+++ kraken2.sh
@@ -1,5 +1,5 @@
-https://github.com/jenniferlu717/KrakenTools#kreport2mpapy
-Download Database - https://ftp.ebi.ac.uk/pub/databases/metagenomics/mgnify_genomes/human-gut/v2.0.2/kraken2_db_uhgg_v2.0.2/
+#github- https://github.com/jenniferlu717/KrakenTools#kreport2mpapy
+#Download Database - https://ftp.ebi.ac.uk/pub/databases/metagenomics/mgnify_genomes/human-gut/v2.0.2/kraken2_db_uhgg_v2.0.2/
 
 #kraken2.sh
 
