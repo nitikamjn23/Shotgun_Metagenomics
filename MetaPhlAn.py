b9923a5fe76ee056c0a12ddb9eb49d6f6a72c5d1
nitikamjn23 <nitika.mjn23@gmail.com>
2026-08-08 16:10:26 +0200

    Update MetaPhlAn.py

--- MetaPhlAn.py
+++ MetaPhlAn.py
@@ -1,1 +1,7 @@
 #github- https://github.com/biobakery/MetaPhlAn/ 
+
+#to download database
+metaphlan --install
+
+#bowtie2 database
+metaphlan --install --db_dir /lustrehome/nitika/bash_script_SRR25132914/metaphlan4/bowtie2db --verbosecftss  
